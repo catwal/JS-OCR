3fbd859c4e54b851b489ac96297bdde59e30ed1f
kitekate <cwallez@hotmail.fr>
2018-02-28 18:58:15 +0100

    ajout de l'exo got avec liste selection

--- public_html/chap_6-Manipulez les formulaires/js/got.js
+++ public_html/chap_6-Manipulez les formulaires/js/got.js
@@ -21,15 +21,51 @@ var maisons = [
 // Renvoie un tableau contenant quelques personnages d'une maison
 function getPersonnages(codeMaison) {
     switch (codeMaison) {
-    case "ST":
-        return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
-    case "LA":
-        return ["Tywin", "Cersei", "Jaime", "Tyrion"];
-    case "BA":
-        return ["Robert", "Stannis", "Renly"];
-    case "TA":
-        return ["Aerys", "Daenerys", "Viserys"];
-    default:
-        return [];
+        case "ST":
+            return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
+        case "LA":
+            return ["Tywin", "Cersei", "Jaime", "Tyrion"];
+        case "BA":
+            return ["Robert", "Stannis", "Renly"];
+        case "TA":
+            return ["Aerys", "Daenerys", "Viserys"];
+        default:
+            return [];
     }
 }
+//cree et renvoi un element HTML -option
+function creerEltOption(texte, valeur) {
+    var elt = document.createElement("option");
+    elt.textContent = texte;
+    elt.value = valeur;
+    return elt;
+}
+
+//cree et renvoi un element HTML -li
+function creerEltLi(texte) {
+    var elt = document.createElement("li");
+    elt.textContent = texte;
+    return elt;
+}
+
+//gestion de la liste deroulante
+//defintion
+var maisonElt = document.querySelector("select");
+//rajout ds la liste deroulante
+maisons.forEach(function(maison) {
+    maisonElt.appendChild(creerEltOption(maison.nom, maison.code));
+});
+
+//fonction pour creer le changement selon le choix de maison
+maisonElt.addEventListener("change", function(event) {
+    //la valeur est le code de maison
+    var persos = getPersonnages(event.target.value);
+    var persosElt = document.getElementById("persos");
+    //vidage de la liste
+    persosElt.innerHTML = "";
+
+
+    persos.forEach(function(perso) {
+        persosElt.appendChild(creerEltLi(perso));
+    })
+})
